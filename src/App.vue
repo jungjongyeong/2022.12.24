e52f1f3d0600c48df49ec135663ef71a0e14b0d0
jjy jeong <asde621@naver.com>
2022-12-25 18:45:55 +0900

    commit2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  안녕하세요
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <button @click="모달창열렸니 = false">닫기</button>
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
+  </div> 
+
+  <div class="menu">
+    <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+  </div>
+
+  <div>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{ 상품들[0] }}</h4>
+    <P>{{ price2 }}</P>
+    <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4>{{ 상품들[1] }}</h4>
+    <P>{{ price2 }}</P>
+    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
+  </div>
+  <div>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4>{{ 상품들[2] }}</h4>
+    <P>{{ price2 }}</P>
+    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      모달창열렸니 : false,
+      메뉴들 : ['Home', 'shop', 'About'],
+      신고수 : [0,0,0],
+      상품들 : ['상품1', '상품2', '상품3'],
+    }
+  },
+  
+  methods : {
+    increase(){
+      this.신고수 ++;
+    },
+  },
 }
 </script>
 
 <style>
+body {
+  margin : 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +82,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 15px;
 }
 </style>
